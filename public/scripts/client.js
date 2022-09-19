6aaa36fc45e7db785b72cf498bf7f2b9e93efc14
Tommy Tran <haitran1995@gmail.com>
2022-09-19 17:17:36 -0400

    Add new tweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,73 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(() => {
 
+  const $tweetContainer = $('#tweet-container');
+
+  const fetchTweets = () => {
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:8080/tweets',
+      success: (data) => {
+        console.log(`data we fetched`, data);
+        $tweetContainer.empty();
+
+        for (const tweet of data) {
+          const $tweet = renderTweet(tweet);
+
+          $tweetContainer.prepend($tweet);
+        }
+      }
+    })
+  };
+
+  fetchTweets();
+
+  const renderTweet = (tweet) => {
+    $('time.timeago').timeago();
+    const $tweet = $(`
+      <header class="tweet-header">
+          <div>
+            <img class="avatars"src=${tweet.user.avatars} />
+            <h2 class="name"> ${tweet.user.name}</h2>
+          </div>
+          <small class="handle">${tweet.user.handle}</small>
+      </header>
+
+          <div class="tweet-body"> </p>
+          <p class="text">${tweet.content.text}</p>
+          </div>
+
+      <footer class="tweet-footer">
+        <small class ="footer-days">${tweet.created_at}</small>
+        <span class="footer-icons">
+          <a href="#"><i class="fa fa-flag"></i></a>
+          <a href="#"><i class="fa fa-retweet"></i></a>
+          <a href="#"><i class="fa fa-heart"></i></a>
+        </span>
+      </footer>
+      `)
+
+    return $tweet
+  };
+
+  const $form = $('#new-tweet');
+
+  $form.on('submit', (event) => {
+    event.preventDefault();
+
+    if(!tweet.content.text.length() < 4) {
+    alert("hello")
+  } 
+
+    const serializedData = $form.serialize();
+    console.log(serializedData);
+
+    $.post('/tweets', serializedData, (response) => {
+      console.log(response);
+    });
+
+    fetchTweets();
+  });
+
+}); // End of Doc.ready.
+
+//         <small class ="footer-days">${tweet.created_at}</small>
